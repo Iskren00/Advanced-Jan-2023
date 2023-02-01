e93b2cca42eaa93a59bd41a7cc6ca10722b6ab40
Iskren00 <iskren002@gmail.com>
2023-02-01 21:56:18 +0200

    Upload
    Tasks from Defining Classes - Exercise .Net6.0

---
+++ DefiningClasses-Exercise/01.DefineAClassPerson/Family.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+
+        public List<Person> People { get { return people; } }
+
+        public void AddMember(Person member) 
+        { 
+            people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.MaxBy(p => p.Age);
+        }
+
+        public List<Person> GetAllOver30(List<Person> persons)
+        {
+            List<Person> result = new (persons);
+
+            foreach (var person in persons)
+            {
+                if (person.Age <= 30)
+                {
+                    result.Remove(person);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DefiningClasses-Exercise/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Family family = new();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] personArg = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Person person = new(personArg[0], int.Parse(personArg[1]));
+
+            family.AddMember(person);              
+        }
+
+        //Person oldestPerson = family.GetOldestMember();
+
+        //Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+        List<Person> result = family.GetAllOver30(family.People);
+
+        result = result.OrderBy(p => p.Name)
+            .ToList();
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.Name} - {item.Age}");
+        }
+    }
+}
